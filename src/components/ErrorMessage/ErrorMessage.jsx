0852d51e6a077c6352a8f2087470f4e54bd9b6d7
NickDev90 <nickie05021990@gmail.com>
2022-08-07 12:09:25 +0200

    api error catch, first hoc

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,12 @@
+import styles from './ErrorMessage.module.css';
+
+const ErrorMessage = () => {
+
+  return (
+    <>
+        <p className={styles.text}>Something gone wrang</p>
+    </>
+  );
+}
+
+export default ErrorMessage;
--- src/pages/PeoplePage/PeoplePage.jsx
+++ src/pages/PeoplePage/PeoplePage.jsx
@@ -1,41 +1,53 @@
 import { useEffect, useState } from 'react';
 import PeopleList from '../../components/PeopleList/PeopleList';
 import { API_PEOPLE } from '../../constants/api';
+import { withErrorApi } from '../../hoc-helpers/withErrorApi';
 import { getPeopleId, getPeopleImage } from '../../services/getPeopleData';
 import { getApiResource } from '../../utils/network';
 import styles from './PeoplePage.module.css';
 
 
-const PeoplePage = () => {
+const PeoplePage = ({setErrorApi}) => {
 
     const [people, setPeople] = useState();
+    
 
     const getResource = async (url) => {
         const res = await getApiResource(url);
-        
-        const peopleList = res.results.map( ({name, url}) => {
-            const id = getPeopleId(url);
-            const img = getPeopleImage(id);
 
-            return {
-                id,
-                name,
-                img
-            }
-        })
-        setPeople(peopleList);
+        if (res) {
+            const peopleList = res.results.map(({ name, url }) => {
+                const id = getPeopleId(url);
+                const img = getPeopleImage(id);
+
+                return {
+                    id,
+                    name,
+                    img
+                }
+            })
+            setPeople(peopleList);
+            setErrorApi(false);
+        } else {
+            setErrorApi(true);
+        }
+
     }
 
     useEffect (() => {
         getResource(API_PEOPLE);
     }, [])
 
-  return (
-    <div className={styles.peoplelist}>
-        <h1>Hi, people!</h1> 
-        { people && <PeopleList people={people}/> }
-    </div>
-  );
+    return (
+
+        <div className={styles.peoplelist}>
+            <h1>Hi, people!</h1>
+            {people && <PeopleList people={people} />}
+
+        </div>
+
+
+    )
 }
 
-export default PeoplePage;
+export default withErrorApi(PeoplePage);
